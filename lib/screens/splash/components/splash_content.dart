a5e1d39f1fbe57c65cecdf8af50ced6af06481a6
ArsalanRehman <60357978+ArsalanRehman@users.noreply.github.com>
2021-06-04 07:48:48 +0300

    Update splash_content.dart

--- lib/screens/splash/components/splash_content.dart
+++ lib/screens/splash/components/splash_content.dart
@@ -17,7 +17,7 @@ class SplashContent extends StatelessWidget {
       children: <Widget>[
         Spacer(),
         Text(
-          "E-ticaret",
+          "N-63",
           style: TextStyle(
             fontSize: getProportionateScreenWidth(36),
             color: kPrimaryColor,
